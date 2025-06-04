a9d700c2f36a9be1f312c18d2ae7b76cc4368c3d
Tony Wang <tony94042@gmail.com>
2025-06-04 16:32:55 +1000

    Added regions create

--- NZWalks.API/obj/Debug/net8.0/NZWalks.API.AssemblyInfo.cs
+++ NZWalks.API/obj/Debug/net8.0/NZWalks.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NZWalks.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+37f217c91b80fbe0fd12201124c00bed3e894168")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e0ed331d03e2890fbc69895ff2d99877099c6865")]
 [assembly: System.Reflection.AssemblyProductAttribute("NZWalks.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NZWalks.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
