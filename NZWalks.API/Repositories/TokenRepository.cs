79682098e16552b0253f96414869c781e515364f
Tony Wang <tony94042@gmail.com>
2025-06-23 20:52:53 +1000

    Start to implement JWT token

---
+++ NZWalks.API/Repositories/TokenRepository.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.Security.Claims;
+
+namespace NZWalks.API.Repositories
+{
+    public class TokenRepository: ITokenRepository
+    {
+        public string CreateJWTToken(IdentityUser user, List<string> Roles)
+        {
+            //Create Claims from Roles
+            var claims = new List<Claim>();
+            Claim.Add(new Claim(ClaimTypes.Email, user.email));
+        }
+    }
+}
--- NZWalks.API/obj/Debug/net8.0/NZWalks.API.AssemblyInfo.cs
+++ NZWalks.API/obj/Debug/net8.0/NZWalks.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NZWalks.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0d15eb55fd1d7f274d7ac445aad33c5b69bef3d8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+16a3c4a5a09040a77a2bba5f7e9bdc5f66d42144")]
 [assembly: System.Reflection.AssemblyProductAttribute("NZWalks.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NZWalks.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
