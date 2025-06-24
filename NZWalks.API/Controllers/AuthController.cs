cda03085545ffe29c19c424670570aa898d4819e
Tony Wang <tony94042@gmail.com>
2025-06-24 20:52:42 +1000

    JWT Token generation

--- NZWalks.API/Controllers/AuthController.cs
+++ NZWalks.API/Controllers/AuthController.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using NZWalks.API.Models.DTO;
+using NZWalks.API.Repositories;
 
 namespace NZWalks.API.Controllers
 {
@@ -10,10 +11,12 @@ namespace NZWalks.API.Controllers
     public class AuthController : ControllerBase
     {
         private readonly UserManager<IdentityUser> userManager;
+        private readonly ITokenRepository tokenRepository;
 
-        public AuthController(UserManager<IdentityUser>userManager)
+        public AuthController(UserManager<IdentityUser>userManager, ITokenRepository tokenRepository)
         {
             this.userManager = userManager;
+            this.tokenRepository = tokenRepository;
         }
         [HttpPost]
         [Route("Register")]
@@ -53,7 +56,19 @@ namespace NZWalks.API.Controllers
 
                 if (checkPasswordResult)
                 {
-                    return Ok("It Works");
+                    var roles = await userManager.GetRolesAsync(user);
+
+                    if (roles != null) 
+                    { 
+                        var jwtToken = tokenRepository.CreateJWTToken(user, roles.ToList());
+
+                        var response = new LoginReponseDto
+                        {
+                            JwtToken = jwtToken,
+                        };
+                        return Ok(response);
+                    }
+
                 }
             }
             return BadRequest("User or password incorrect");
