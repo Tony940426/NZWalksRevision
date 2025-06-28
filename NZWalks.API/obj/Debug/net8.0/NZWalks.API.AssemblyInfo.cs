2b95c141cfbd9a57f6427f87a014a26b39834d26
Tony Wang <tony94042@gmail.com>
2025-06-28 14:27:15 +1000

    Added auth to Swagger

--- NZWalks.API/obj/Debug/net8.0/NZWalks.API.AssemblyInfo.cs
+++ NZWalks.API/obj/Debug/net8.0/NZWalks.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NZWalks.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aa31cc620eb35c404713478cca0d1a03a4ad03ca")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+809179a130b4e0c85dbdc00aed644d6f2ef72a87")]
 [assembly: System.Reflection.AssemblyProductAttribute("NZWalks.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NZWalks.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
