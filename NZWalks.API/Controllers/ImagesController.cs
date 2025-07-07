32d3f8f6cf69c1d1a0d87d499498e27869270112
Tony Wang <tony94042@gmail.com>
2025-07-07 20:30:23 +1000

    Implemented image controller, interface, repo logic

--- NZWalks.API/Controllers/ImagesController.cs
+++ NZWalks.API/Controllers/ImagesController.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using NZWalks.API.Models.Domain;
 using NZWalks.API.Models.DTO;
+using NZWalks.API.Repositories;
 
 namespace NZWalks.API.Controllers
 {
@@ -8,7 +10,12 @@ namespace NZWalks.API.Controllers
     [ApiController]
     public class ImagesController : ControllerBase
     {
-        const int maxFileLength = 10485760;
+        private readonly IImageRepository imageRepository;
+
+        public ImagesController(IImageRepository imageRepository)
+        {
+            this.imageRepository = imageRepository;
+        }
 
         [HttpPost]
         [Route("Upload")]
@@ -18,7 +25,18 @@ namespace NZWalks.API.Controllers
 
             if (ModelState.IsValid)
             {
+                var imageDomainModel = new Image
+                {
+                    File = request.File,
+                    FileExtension = Path.GetExtension(request.File.FileName),
+                    FileSizeInBytes = request.File.Length,
+                    FileName = request.FileName,
+                    FileDescription = request.FileDescription
+                };
 
+                await imageRepository.Upload(imageDomainModel);
+
+                return Ok(imageDomainModel);
             }
 
             return BadRequest(ModelState);
@@ -26,6 +44,7 @@ namespace NZWalks.API.Controllers
 
         private void ValidateFileUpload(ImageUploadRequestDto request)
         {
+            const int maxFileLength = 10485760;
             var allowedExtensions = new string[] { ".jpg", ".jped", ".png" };
 
             if (!allowedExtensions.Contains(Path.GetExtension(request.File.FileName)))
--- NZWalks.API/Program.cs
+++ NZWalks.API/Program.cs
@@ -14,6 +14,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddHttpContextAccessor();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(options => 
@@ -49,8 +50,11 @@ builder.Services.AddDbContext<NZWalksDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("NZWalksConnectionString")));
 builder.Services.AddDbContext<NZWalksAuthDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("NZWalksAuthConnectionString")));
+
 builder.Services.AddScoped<IRegionRepository, SQLRegionRepository>();
 builder.Services.AddScoped<IWalkRepository, SQLWalkRepository>();
+builder.Services.AddScoped<IImageRepository, LocalImageRepository>();
+
 builder.Services.AddAutoMapper(typeof(AutoMapperProfiles));
 builder.Services.AddScoped<ITokenRepository, TokenRepository>();
 
